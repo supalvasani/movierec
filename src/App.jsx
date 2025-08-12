0e7fcc61890d582127ac9a399ff87a57a9d57b60
supalvasani <supalvasani@gmail.com>
2025-08-12 14:06:18 +0530

    dashboard done!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Search from "./Components/Search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
     return (
-        <div>App</div>
+        <main>
+        <div className= "pattern" />
+        <div className= "wrapper">
+            <header>
+                <img src="./hero.png" alt={"Hero Banner"}/>
+                <h1>Find <span className="text-gradient">Movies</span>You'll Enjoy Without Hassle!!</h1>
+            </header>
+
+            <Search searchTerm = {searchTerm} setSearchTerm={setSearchTerm} />
+        </div>
+        </main>
     )
 }
 export default App
