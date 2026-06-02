ccd67a008278c541dec0a90cb8b6ca70dd1f5371
Supal Vasani <supalvasani@gmail.com>
2026-06-02 13:34:23 +0530

    update

--- src/services/appwrite.js
+++ src/services/appwrite.js
@@ -33,7 +33,7 @@ export const updateSearchCount = async (searchTerm, movie) => {
                 searchTerm,
                 count: 1,
                 movie_id: movie.id,
-                poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+                poster_url: movie.poster_url || movie.poster_path || '',
             });
         }
     } catch (e) {
